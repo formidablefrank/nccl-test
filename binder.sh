c1782556360efc995565c64675884fb685154288
J Franco Ray <5762748+formidablefrank@users.noreply.github.com>
2026-06-01 03:25:48 +0200

    use local rank instead of global rank

--- binder.sh
+++ binder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-case $(( ${OMPI_COMM_WORLD_RANK} )) in
+case $(( ${OMPI_COMM_WORLD_LOCAL_RANK} )) in
     0) export UCX_NET_DEVICES=mlx5_0:1 CUDA_VISIBLE_DEVICES=0 ;;
     1) export UCX_NET_DEVICES=mlx5_1:1 CUDA_VISIBLE_DEVICES=1 ;;
     2) export UCX_NET_DEVICES=mlx5_2:1 CUDA_VISIBLE_DEVICES=2 ;;
